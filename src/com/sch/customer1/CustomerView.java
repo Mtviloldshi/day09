eaeefba6e3a2bee0668d4587a19ad705c33c4b91
794690923@qq.com <794690923@qq.com>
2020-09-02 23:53:58 +0800

    修复BUG

--- src/com/sch/customer1/CustomerView.java
+++ src/com/sch/customer1/CustomerView.java
@@ -4,6 +4,10 @@ public class CustomerView {
     CustomerList customerList = new CustomerList(10);
     boolean flag =true ;
     public void enterMainmenu(){
+        Cusomer cust = new Cusomer("1",'1',12,"1","1");
+        Cusomer cust2 = new Cusomer("233",'2',12,"1","1");
+        customerList.addCustomer(cust);
+        customerList.addCustomer(cust2);
         do {
             System.out.println("---------- 信息统计 ----------");
             System.out.println("--------------1--------------");
@@ -17,10 +21,13 @@ public class CustomerView {
                     addNewCustomer();
                     break;
                 case '2':
+                    deleteCustomer();
                     break;
                 case '3':
+                    modifyCustomer();
                     break;
                 case '4':
+                    listAllCustomer();
                     break;
                 case '5':
                     flag = false;
@@ -42,12 +49,50 @@ public class CustomerView {
         Cusomer c= new Cusomer(name,gender,age,phone,email);
         customerList.addCustomer(c);
     }
+
+    /**
+     * 修改
+     */
     private void modifyCustomer(){
+        System.out.println("---输入修改编号--------");
+        int num = CMUtility.readInt();
+        if(num<0 || num >customerList.getAllCustomers().length) {
+            return;
+        }else{
+                System.out.println("开始修改1");
+                String name = CMUtility.readString(10);
+                System.out.print("2");
+                char gender = CMUtility.readChar('1');
+                System.out.print("3");
+                int age = CMUtility.readInt(10);
+                System.out.print("4");
+                String phone = CMUtility.readString(20);
+                System.out.print("5");
+                String email = CMUtility.readString(20);
+                Cusomer c = new Cusomer(name, gender, age, phone, email);
+            boolean flag =customerList.replaceCustomer(num,c);
+            System.out.println(flag +"修改了第" + (num)+"项，修改为 "+ c.getName() );
+            }
 
+        //找谁去替换
+//        1
     }
-    private void deleteCustomer(){}
+    private void deleteCustomer(){
+        System.out.println("请输入你想要删除的项目");
+        int num = CMUtility.readInt();
+        if(num<0 || num >customerList.getAllCustomers().length) {
+            return;
+        }else {
+            customerList.deleteCustomer(num);
+        }
+    }
     private void listAllCustomer(){
-
+        System.out.println("--------------------");
+        Cusomer[] c = customerList.getAllCustomers();
+        for (int i = 0 ; i<c.length;i++){
+            System.out.println(i+"\t"+c[i].getName());
+        }
+        System.out.println("列表循环结束");
     }
 
     public static void main(String[] args) {
